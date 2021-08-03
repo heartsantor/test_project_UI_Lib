5e0f28ef64dd5806d3f33f026d2808a82dd549df
heartsantor <antorboss.bd@gmail.com>
2021-08-03 19:47:15 +0600

    create:pack

---
+++ src/layout/Layout.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <p>jhgjhg</p>
+            <Header>
+                {children}
+            </Header>
+        </div>
+    )
+}
+
+export default Layout
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,23 @@
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Layout from '../layout/Layout'
 import '../styles/globals.css'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#663399'
+    }
+  }
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
