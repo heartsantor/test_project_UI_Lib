6788701dd5a0fd6ab7cecfcdc8a7d6ecf22dd5c9
heartsantor <antorboss.bd@gmail.com>
2021-08-08 13:41:04 +0600

    fix: styleled

--- src/components/CustromCard.js
+++ src/components/CustromCard.js
@@ -2,25 +2,33 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
+import { styled, alpha } from '@material-ui/core/styles';
+
 import styles from '../styles/card.module.css'
 
-const CustromCard = ({icon,title,amount,color,colorlight}) => {
+const StyleCard = styled(Card)(({ theme }) => ({
+    margin: '5px 12px',
+    boxShadow: 'none',
+    borderRadius: '24px',
+    cursor: 'pointer',
+}));
 
+const CustromCard = ({ icon, title, amount, color, colorlight }) => {
 
     return (
-        <Card style={{ boxShadow: 'none',margin:'5px 12px',borderRadius:'24px' }}>
+        <StyleCard>
             <CardContent>
                 <div className={styles.cardwrapper}>
-                    <div className={styles.persion} style={{backgroundColor:colorlight}}>
-                    {icon}
+                    <div className={styles.persion} style={{ backgroundColor: colorlight }}>
+                        {icon}
                     </div>
                     <div className={styles.textwrapper}>
                         <p>{title}</p>
-                        <span style={{color:color}}>{amount}</span>
+                        <span style={{ color: color }}>{amount}</span>
                     </div>
                 </div>
             </CardContent>
-        </Card>
+        </StyleCard>
     )
 }
 
